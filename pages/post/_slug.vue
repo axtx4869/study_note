4b610b63e66a0b22a1765b0ade8f1d0180b56579
aptx4869 <shota1445@icloud.com>
2021-01-24 11:38:16 +0900

    h3タグの下にunderlineを出すようにした

--- pages/post/_slug.vue
+++ pages/post/_slug.vue
@@ -1,25 +1,95 @@
 <template>
-  <div>
+  <section>
     <div class="bg-yellow-300 w-2/5 opacity-25 h-12 mg-tape-slug"></div>
-    <section class="bg-white m-4 py-6 sm:px-20 px-3 shadow">
+    <div class="bg-white m-4 py-6 lg:px-16 md:px-12 px-6 shadow">
       <div
-        class="sm:w-11/12 sm:h-64 h-32 my-6 mx-auto bg-contain bg-center bg-no-repeat"
-        :style="
-          'background-image: url(' + post.fields.image.fields.file.url + ')'
-        "
-      ></div>
+        class="md:h-64 sm:h-56 h-40 w-full sm:my-6 my-3 mx-auto object-cover"
+      >
+        <img
+          :src="post.fields.image.fields.file.url"
+          alt="投稿画像"
+          class="h-full m-auto sm:object-cover object-contain"
+        />
+      </div>
       <div class="">
         <h1 class="text-center text-4xl font-fam-comic">
           {{ post.fields.title }}
         </h1>
-        <time class="text-gray-500 block text-right py-2">{{
+        <time class="text-gray-600 block text-right py-2">{{
           getFormattedDate(post.fields.date)
         }}</time>
         <div class="content" v-html="$md.render(post.fields.content)"></div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
+<style>
+.content {
+  font-family: "Comic Neue", sans-serif;
+}
+
+.content h1 {
+  font-size: 1.8rem;
+  margin: 25px 0;
+  border-bottom: 2px solid #000;
+  padding-left: 0.5rem;
+}
+.content h2 {
+  font-size: 1.5rem;
+  margin: 20px 0;
+  border-bottom: 1px solid #eee;
+}
+.content h3 {
+  font-size: 1.2rem;
+  margin: 15px 0;
+  text-decoration: underline;
+  text-underline-position: under;
+}
+
+.content p {
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+}
+
+.content a {
+  color: blue;
+}
+
+.content ol,
+ul {
+  padding-left: 1rem;
+}
+
+.content li {
+  list-style: disc;
+}
+
+.content li:first-line {
+  margin-left: 1rem;
+}
+
+.content code {
+  background: #eee;
+  padding: 2px;
+}
+.content pre code {
+  background: none;
+  padding: 0;
+}
+.content pre {
+  background: #32375d;
+  color: #fff;
+  padding: 5px;
+}
+
+.shadow {
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.22);
+}
+
+.mg-tape-slug {
+  margin: 0 auto -50px 30%;
+}
+</style>
 
 <script>
 import client from "@/plugins/contentful.js";
@@ -54,69 +124,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.content {
-  font-family: "Comic Neue", sans-serif;
-}
-
-.content h1 {
-  font-size: 1.8rem;
-  margin: 25px 0;
-  border-bottom: 2px solid #000;
-  padding-left: 0.5rem;
-}
-.content h2 {
-  font-size: 1.5rem;
-  margin: 20px 0;
-  border-bottom: 1px solid #eee;
-}
-.content h3 {
-  font-size: 1.2rem;
-  margin: 15px 0;
-}
-
-.content p {
-  font-size: 1.2rem;
-  line-height: 1.5rem;
-}
-
-.content a {
-  color: blue;
-}
-
-.content ol,
-ul {
-  padding-left: 1rem;
-}
-
-.content li {
-  list-style: disc;
-}
-
-.content li:first-line {
-  margin-left: 1rem;
-}
-
-.content code {
-  background: #eee;
-  padding: 2px;
-}
-.content pre code {
-  background: none;
-  padding: 0;
-}
-.content pre {
-  background: #000;
-  color: #fff;
-  padding: 5px;
-}
-
-.shadow {
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.22);
-}
-
-.mg-tape-slug {
-  margin: 0 auto -50px 30%;
-}
-</style>
