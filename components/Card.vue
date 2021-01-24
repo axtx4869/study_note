b4b943ce42a0811aa9afe8d6d1b0b2c9b618ed47
aptx4869 <shota1445@icloud.com>
2021-01-24 11:34:45 +0900

    画像をbgからimgに変更。また、日付のカラーを濃くした
    LightHouseで指摘されていたから。コントラスト比等(アクセシビリティ)

--- components/Card.vue
+++ components/Card.vue
@@ -2,29 +2,45 @@
   <nuxt-link
     :to="'/post/' + post.fields.slug"
     class="xl:w-64 lg:w-56 w-10/12 mx-auto bg-white box-shadow hover:bg-blue-100"
+    aria-label="{description}"
   >
-    <article class="lg:m-3">
-      <div class="pl-3 pr-3 pt-3">
-        <div
-          class="mb-3 w-full h-64 bg-center bg-cover bg-height"
-          :style="
-            'background-image:url(' + post.fields.image.fields.file.url + ')'
-          "
-        ></div>
+    <article class="m-3">
+      <div class="h-40">
+        <img
+          :src="post.fields.image.fields.file.url"
+          alt="投稿画像"
+          class="h-40 w-full m-auto object-cover"
+        />
       </div>
-      <div class="h-auto pl-4 pr-4 pb-4">
-        <small class="text-gray-500">{{
+      <div class="h-auto p-4">
+        <time class="text-gray-600">{{
           getFormattedDate(post.fields.date)
-        }}</small>
+        }}</time>
         <br />
-        <small class="block text-xs text-right underline light-blue"
+        <small class="block text-xs text-right underline my-3 tag-color"
           >#{{ post.fields.category.fields.name }}</small
         >
-        <h3 class="font-bold py-2 font-fam-comic">{{ post.fields.title }}</h3>
+        <strong class="font-bold py-4 font-fam-comic">
+          {{ post.fields.title }}
+        </strong>
       </div>
     </article>
   </nuxt-link>
 </template>
+<style>
+.box-shadow {
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.22);
+}
+
+.box-shadow:active {
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.11);
+  transform: translateY(2px);
+}
+
+.tag-color {
+  color: royalblue;
+}
+</style>
 
 <script>
 export default {
@@ -50,22 +66,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.box-shadow {
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.22);
-}
-
-.box-shadow:active {
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.11);
-  transform: translateY(2px);
-}
-
-.bg-height {
-  height: 200px;
-}
-
-.light-blue {
-  color: #0bd;
-}
-</style>
