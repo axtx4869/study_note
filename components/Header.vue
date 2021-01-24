0b080ecade807de09ef40eb0b3dc4cbffb01b958
aptx4869 <shota1445@icloud.com>
2021-01-24 11:42:02 +0900

    サブタイトルの色を変更した
    LightHouseコントラストについて警告が出ていたから

--- components/Header.vue
+++ components/Header.vue
@@ -7,18 +7,20 @@
         STUDY NOTE
       </h1>
     </nuxt-link>
-    <p class="text-white px-2 pt-2 pb-4">
+    <p class="subtitle-color px-2 pt-2 pb-4">
       エンジニアを目指す大学生のアウトプット用ブログ
     </p>
   </div>
 </template>
-<script>
-export default {};
-</script>
-
 <style>
 .header-title {
   -webkit-text-stroke: 3px #555555;
   text-shadow: 4px 4px #498ee0;
 }
+.subtitle-color {
+  color: #4a5568;
+}
 </style>
+<script>
+export default {};
+</script>
